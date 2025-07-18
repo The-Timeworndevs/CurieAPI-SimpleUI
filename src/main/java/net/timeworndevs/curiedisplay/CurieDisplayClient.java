1c6a9338c5a55e18e8bf74bab385e39494daa29b
ItsToastCraft <itstoastcraft@gmail.com>
2025-07-18 17:37:34 -0400

    UI update
    Updated the texture used by the UI
    
    removed unnecessary textures
    
    Use modrinth maven to get project

--- src/main/java/net/timeworndevs/curiedisplay/CurieDisplayClient.java
+++ src/main/java/net/timeworndevs/curiedisplay/CurieDisplayClient.java
@@ -1,20 +1,23 @@
-package net.timeworndevs.quantumui;
+package net.timeworndevs.curiedisplay;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.timeworndevs.quantumui.client.RadiationHudOverlay;
+import net.timeworndevs.curiedisplay.client.RadiationHudOverlay;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Environment(EnvType.CLIENT)
-public class QuantumUIClient implements ClientModInitializer {
+public class CurieDisplayClient implements ClientModInitializer {
+    public static final String MOD_ID = "curie-display";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
     public void onInitializeClient() {
         ClientLifecycleEvents.CLIENT_STARTED.register((client) -> {
             RadiationHudOverlay.init();
             for (RadiationHudOverlay.Overlay overlay: RadiationHudOverlay.overlays) {
-                QuantumUI.LOGGER.debug(overlay.toString());
                 HudRenderCallback.EVENT.register(overlay);
             }
         });
